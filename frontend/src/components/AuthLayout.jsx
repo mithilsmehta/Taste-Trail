24c781dbff96bc5530d1ad9be2c0d1be73ca530f
mithilmehta <mithilmehta15@gmail.com>
2026-05-29 21:48:13 +0530

    login page left image slider not showing image changed code for that

--- frontend/src/components/AuthLayout.jsx
+++ frontend/src/components/AuthLayout.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "../styles/auth.css";
 
-const images = [
-  "/src/assets/img1.jpg",
-  "/src/assets/img2.jpg",
-  "/src/assets/img3.jpg",
-  "/src/assets/img4.jpg",
-  "/src/assets/img5.jpg",
-  "/src/assets/img6.jpg",
-];
+import img1 from "../assets/img1.jpg";
+import img2 from "../assets/img2.jpg";
+import img3 from "../assets/img3.jpg";
+import img4 from "../assets/img4.jpg";
+import img5 from "../assets/img5.jpg";
+import img6 from "../assets/img6.jpg";
+
+const images = [img1, img2, img3, img4, img5, img6];
 
 export default function AuthLayout({ children }) {
   const [index, setIndex] = useState(0);
